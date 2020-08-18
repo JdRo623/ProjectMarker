5bc7164cec60c0328d63196b4dd1eee80f688843
Joan Duarte <JdRodriguez623@outlook.com>
2020-08-17 22:52:19 -0500

    Hot Fix

--- FRONTEND/PIC V2/src/views/app/pic/util.js
+++ FRONTEND/PIC V2/src/views/app/pic/util.js
@@ -23,30 +23,8 @@ import Utils from "../../../util/Util";
 class Util extends Component {
   constructor(props) {
     super(props);
-    this.validateEmail = this.validateEmail.bind(this);
-    this.validateName = this.validateName.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  validateEmail(value) {
-    let error;
-    if (!value) {
-      error = "Please enter your email address";
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
-      error = "Invalid email address";
-    }
-    return error;
-  }
-
-  validateName(value) {
-    let error;
-    if (!value) {
-      error = "Please enter your name";
-    } else if (value.length < 2) {
-      error = "Value must be longer than 2 characters";
-    }
-    return error;
-  }
 
   render() {
     return (
